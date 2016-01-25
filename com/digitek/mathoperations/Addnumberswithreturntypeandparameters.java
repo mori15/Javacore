21572f2bb9a0b6d7fc9d5ceb9580e3966b58e4a6
mori15 <mms@MMSs-MacBook-Pro.local>
2016-01-25 13:39:54 -0600

    3
    3

---
+++ com/digitek/mathoperations/Addnumberswithreturntypeandparameters.java
@@ -0,0 +1,24 @@
+package com.digitek.mathoperations;
+
+public class Addnumberswithreturntypeandparameters {
+
+	public static int addNumbersWithReurnTypeAndAcceptParameters(int a ,int b){
+
+		int c = a+b;
+		System.out.println("Result from addition with return type w parameters :" + c);
+		
+		return c;
+	}
+
+
+	
+	public static void main(String args[]){
+		int result1 = addNumbersWithReurnTypeAndAcceptParameters(10, 15);
+		int result2 = addNumbersWithReurnTypeAndAcceptParameters(40, 60);
+		int result3 = addNumbersWithReurnTypeAndAcceptParameters(20, 30);
+	}
+		
+	
+
+}
+
